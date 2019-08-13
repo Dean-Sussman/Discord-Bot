6e8f1dd3072f254e463408967cec6dcea0a4a7a7
Dean-Sussman <43920592+Dean-Sussman@users.noreply.github.com>
2019-08-13 16:23:14 -0400

    Add files via upload

--- SmiteBot.py
+++ SmiteBot.py
@@ -5,12 +5,13 @@ from discord.ext import commands
 
 bot = commands.Bot(".")
 
-general_channel_id = 00000000000000
-alive_role_id = 000000000000000
-mafia_channel_id = 00000000000000
-guild_id = 000000000000000
+general_channel_id = 000000000000000
+alive_role_id = 0000000000000000
+mafia_channel_id = 0000000000000
+guild_id = 00000000000000
+mafia_dm_channel_id = 0000000000
 
-TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXX'
+TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXX'
 
 
 @bot.event
@@ -28,17 +29,16 @@ async def on_message(message):
         if await channel_check(5):
             await mafia(5)
         else:
-            await channel.send('5 players must be in the "Mafia" voice channel for the game to begin.'
+            await channel.send('5 players must be in the "Mafia" voice channel for the game to begin. '
                                'Re-send the command once all players have joined.')
     elif message.content.startswith('~mafia6'):
         if await channel_check(6):
             await mafia(6)
         else:
-            await channel.send('6 players must be in the "Mafia" voice channel for the game to begin.'
+            await channel.send('6 players must be in the "Mafia" voice channel for the game to begin. '
                                'Re-send the command once all players have joined.')
-    if message.guild is None:
-        await channel.send("DM received")
-        # this works YEET; expand on this to get the mafia user's target
+    if message.guild is None and message.content.isdigit() and mafia_dm_channel_id is channel.id:
+        await verify_mafia_target(message.content, channel)
 
 
 @bot.event
@@ -70,6 +70,8 @@ async def mafia(number_of_players):
     mafia_user = await random_human(voice_channel)
     guild = bot.get_guild(guild_id)
     await mafia_user.create_dm()
+    global mafia_dm_channel_id
+    mafia_dm_channel_id = mafia_user.dm_channel.id
     await mafia_user.dm_channel.send("You are the mafia!")
     await channel.send(f'Welcome to Discord Mafia! You have chosen to play with {number_of_players} players. '
                        f'In Discord Mafia, there are {number_of_players - 1} town and 1 mafia. '
@@ -96,9 +98,26 @@ async def mafia(number_of_players):
     await channel.send(f'The first night time begins in 60 seconds. \n'
                        f'The mafia player has been sent a DM with instructions on choosing a target. \n'
                        f'Good luck!')
+    await send_instructions(mafia_user, voice_channel)
     time.sleep(6)
     await mute_channel(voice_channel)
-    # get mafia's target
+
+
+@bot.event
+async def mafia_day_cycle(target, mafia_user):
+    voice_channel = bot.get_channel(mafia_channel_id)
+    channel = bot.get_channel(general_channel_id)
+    await un_mute_channel(voice_channel)
+    await channel.send(f'{target.mention} has been killed by the mafia!')
+    if target.id is mafia_user.id:
+        await channel.send('The mafia is dead; town wins!')
+    alive_members = []
+    for member in voice_channel.members:
+        if member.roles[1].id == alive_role_id:
+            alive_members.append(member)
+    await channel.send(f'These are the remaining living players: {alive_members}')
+    await channel.send('The next day has begun. You have three minutes to discuss.')
+    time.sleep(180)
 
 
 @bot.event
@@ -119,10 +138,10 @@ async def random_human(channel):
 
 
 @bot.event
-async def add_role_to_channel(role_id, channel):
+async def add_role_to_channel(role, channel):
     member_list = channel.members
     for member in member_list:
-        await member.add_roles(role_id)
+        await member.add_roles(role)
 
 
 @bot.event
@@ -130,6 +149,13 @@ async def mute_channel(channel):
     member_list = channel.members
     for member in member_list:
         await member.edit(mute=True)
+
+
+@bot.event
+async def un_mute_channel(channel):
+    member_list = channel.members
+    for member in member_list:
+        await member.edit(mute=False)
 
 
 @bot.event
@@ -141,7 +167,26 @@ async def send_instructions(mafia_user, mafia_channel):
     await mafia_user.dm_channel.send('These are the players you can target tonight: ')
     await mafia_user.dm_channel.send(alive_members)
     await mafia_user.dm_channel.send('In order to target a player, DM me with their position in the list.')
-    await mafia_user.dm_channel.send(f'For example, to vote for {alive_members[0]}, send "1". To vote for {alive_members[1]}, send "2", and so on.')
+    await mafia_user.dm_channel.send(f'For example, to vote for {alive_members[0]}, send "1" (without quotes). ')
+                                     #f'To vote for {alive_members[1]}, send "2", and so on.')
+
+
+@bot.event
+async def verify_mafia_target(target, dm_channel):
+    alive_members = []
+    choice = int(target, 10)
+    channel = bot.get_channel(mafia_channel_id)
+    for member in channel.members:
+        if member.roles[1].id == alive_role_id:
+            alive_members.append(member)
+    if choice > len(alive_members) or choice <= 0:
+        await dm_channel.send(f'You cannot target that player. You must send a number between 1 and {len(alive_members)}.')
+    else:
+        await dm_channel.send(f'Confirmed, targeting {alive_members[choice-1].display_name}')
+        guild = bot.get_guild(guild_id)
+        role = guild.get_role(alive_role_id)
+        await alive_members[choice-1].remove_roles(role)
+        await mafia_day_cycle(alive_members[choice-1], dm_channel.recipient)
 
 
 bot.run(TOKEN)
